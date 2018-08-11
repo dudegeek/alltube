33e8e6eb4f046ea831caa27a2c60002ffcbbc1ee
Priyank Vaghela <priyankvaghela11@gmail.com>
2018-08-11 11:59:35 +0000

    Update Index

--- controllers/FrontController.php
+++ controllers/FrontController.php
@@ -465,7 +465,8 @@ class FrontController
         } else {
             if (empty($videoUrls[0])) {
                 throw new \Exception(_("Please enter a valid URL like: https://www.youtube.com/watch?v=n1VJ39nVIBk
-Note: Playlist URLs like https://www.youtube.com/watch?v=n1VJ39nVIBk&index=5&list=RDMMyP9KiFTyBks are not supported."));
+
+Note: URLs like https://www.youtube.com/watch?v=n1VJ39nVIBk&index=5&list=RDMMyP9KiFTyBks are not supported."));
             }
 
             return $response->withRedirect($videoUrls[0]);
